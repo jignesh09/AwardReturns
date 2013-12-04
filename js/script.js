5fcf4383365713de1b668bbcf8be3de755967acd
Jignesh <nirav.changela@phpprogrammers.com>
2013-12-04 10:24:47 +0530

    BarcodeScanner Changed

--- js/script.js
+++ js/script.js
@@ -1769,7 +1769,20 @@ function gotoSettingpage() {
 
 function starBarScanning(){
 //alert('ok')
-alert(window.plugins.barcodeScanner)
+   var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+alert(scanner);
+   scanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
+
  window.plugins.barcodeScanner.scan(
       function (result) {
             //alert(result.text);
